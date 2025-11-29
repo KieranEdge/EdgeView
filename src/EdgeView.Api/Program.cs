93a59d10cc3f8aa75228e7fa27f10cff09e6c93f
Kieran Edge <kesedge94@gmail.com>
2025-11-29 23:29:14 +0000

    tidy up

--- src/EdgeView.Api/Program.cs
+++ src/EdgeView.Api/Program.cs
@@ -7,7 +7,6 @@ var builder = WebApplication.CreateBuilder(args);
 // 1. Register services BEFORE Build()
 builder.Services.AddScoped<IBinScraper, BarnsleyBinScraper>();
 builder.Services.AddScoped<IBinDayService, BinDayService>();
-
 // 2. Add controllers (if using attribute routing)
 builder.Services.AddControllers();
 
